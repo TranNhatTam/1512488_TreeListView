c00138c061cc22ee6b01a60d996f627a33f43b98
DESKTOP-9MGAKJ9\Tam <nhattam231297@gmail.com>
2017-10-22 22:17:19 +0700

    source code

---
+++ Lab06/Lab06/resource.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Lab06.rc
+//
+#define IDC_MYICON                      2
+#define IDI_DESKTOP                     3
+#define IDI_MYCOMPUTER                  4
+#define IDD_LAB06_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_LAB06                       107
+#define IDI_SMALL                       108
+#define IDC_LAB06                       109
+#define IDL_LISTVIEW                    110
+#define IDT_TREEVIEW                    111
+#define IDR_MAINFRAME                   128
+#define IDI_DESKTOP                     140
+#define IDI_MYCOMPUTER                  141
+#define IDI_DRIVE                       142
+#define IDC_STATUSBAR                   143
+#define ID_FILE_LOADSIZE                144
+#define ID_FILE_UPDATESIZE                  145
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
